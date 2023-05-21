ba710e51d9393e8871dc10a5fd4615b6d42f2b13
Questi0nM4rk <kubalozy@seznam.cz>
2023-05-21 18:07:11 +0200

    🐱‍💻🐱‍👤

---
+++ dollar_to_inr/dollar_to_inr.py
@@ -0,0 +1,9 @@
+# * 79
+
+def dollar_to_inr(dollars: float, conversion: float) -> float:
+    return dollars * conversion
+
+rate = 79
+amount = 4
+
+print(dollar_to_inr(amount, rate)) # output: 316
