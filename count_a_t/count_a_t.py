727162a50b2608eb2f3c049377aed40520cf0d3b
Questi0nM4rk <kubalozy@seznam.cz>
2023-05-21 19:20:17 +0200

    🐱‍💻🐱‍👤

--- count_a_t/count_a_t.py
+++ count_a_t/count_a_t.py
@@ -4,11 +4,12 @@ def easy_apr(text: str) -> tuple[int, int]:
     text = text.lower()
     return (text.count("t"), text.count("e"))
 
+
 def next_apr(text: str) -> dict:
     text = text.lower()
     find = ["t", "e"]
     
-    return {char: sum(char in word for word in text.split()) for char in find}
+    return {char: sum(char in word for word in text) for char in find}
 
 
 with open("sample.txt", "r", encoding="utf-8") as f:
@@ -22,3 +23,11 @@ with open("sample.txt", "r", encoding="utf-8") as f:
     print("Occurrences of 't':", occurrences["t"])
     print("Occurrences of 'e':", occurrences["e"])
     
+    # output:
+    """
+    t/T = 39
+    e/E = 57
+    
+    Occurrences of 't': 39
+    Occurrences of 'e': 57
+    """
