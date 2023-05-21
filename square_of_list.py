b882b10b5dffea74ced3c273c02d552a0dfd5124
Questi0nM4rk <kubalozy@seznam.cz>
2023-05-21 17:24:59 +0200

    🐱‍💻🐱‍👤

--- square_of_list.py
+++ square_of_list.py
@@ -1,3 +1,9 @@
 numbers = [1, 2, 3, 4, 5]
 squares = [x**2 for x in numbers]
 print(squares) # output: [1, 4, 9, 16, 25]
+
+# for input of list:
+
+print([int(x)**2 for x in input().split(",")])
+# input: 1,2,3,4,5,6
+# output: [1, 4, 9, 16, 25, 36]
